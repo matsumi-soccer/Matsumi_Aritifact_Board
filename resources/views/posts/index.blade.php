9ae05d126fd69940b14d249780ab1854bcff5bb5
matsumi-soccer <my18847matsu@softbank.ne.jp>
2022-09-05 15:31:14 +0900

    Artifact_3

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Game掲示板</h1>
+        <div class='posts'>
+            <h3 class='game_title'>APEX</h3>
+            @foreach ($posts as $post)
+            <div class='post'>
+            <!--h3 class='title'>RankID:{{$post->id}}</h3>-->
+                <p class='rank'>Rank:{{$post->title}}</p>
+            </div>
+            @endforeach
+        </div>
+    </body>
+</html
