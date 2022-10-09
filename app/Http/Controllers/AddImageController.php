dc1112ecc382cf5cee3564b0f5b6ceffb0d84ead
matsumi-soccer <my18847matsu@softbank.ne.jp>
2022-10-09 10:56:30 +0900

    image4

--- app/Http/Controllers/AddImageController.php
+++ app/Http/Controllers/AddImageController.php
@@ -10,6 +10,7 @@ use App\Auth;
 
 class AddImageController extends Controller
 {
+    //開発環境only
     // public function addImage(Request $request)
     // {
     //     //Userのプロフィール画像変更
@@ -22,33 +23,30 @@ class AddImageController extends Controller
     //     $file_path = \DB::table('users')->where('id',\Auth::user()->id)->first();
     //     return redirect()->back();
     // }
+    
     public function addImage(Request $request)
     {
         //Userのプロフィール画像変更
         if( app()->isLocal()|| app()->runningUnitTests())
         {
             $file_name = $request->image->getClientOriginalName();
-            $img = $request->image->storeAs('public/profiles', $file_name);  
-            
+            $img = $request->image->storeAs('public/profiles', $file_name);
             DB::table('users')
                 ->where('id', \Auth::user()->id)
                 ->update(['profile_image' => $file_name]);
-            
+    
+            $file_path = \DB::table('users')->where('id',\Auth::user()->id)->first();
+            return redirect()->back();
         }else{
             # 本番環境
-            $image = $request->image->getClientOriginalName();
-            $path = Storage::disk('s3')->put('/', $image, 'public');
-            $url = Storage::disk('s3')->url($path);
+            $file_name = $request->image;
+            Storage::disk('s3')->delete($image);
+            $path = Storage::disk('s3')->putFile('/', $image, 'public');
             DB::table('users')
                 ->where('id', \Auth::user()->id)
-                ->update(['profile_image' => $url]);
+                ->update(['profile_image' => $path]);
+            
         }
-        
-      
-
-        $file_path = \DB::table('users')->where('id',\Auth::user()->id)->first();
-        return redirect()->back();
 
     }
-
 }
